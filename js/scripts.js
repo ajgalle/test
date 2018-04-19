977f58197871639ff5f83297642d70cdf22f5a4f
Andrew <andrew@test.com>
2018-04-19 06:16:35 +0000

    Photo fetching operational

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,40 @@
-//const select = document.getElementById('breeds');
-//const card = document.querySelector('.card');
-//const form = document.querySelector('form');
 
-//fetch("https://dog.ceo/api/breeds/list/all").then(gotInfo,errorFunction);
-alert("operational");
+// first, Lets get all them dogs.
+//const dogChart = document.getElementById("printTarget");
+    //fetch("https://dog.ceo/api/breeds/image/random");
 
-//gotInfo(response)
-//{
-  //console.log(response)
-//}
-//errorFunction()
-//{
-  //alert("Something went wrong.");
-//}
+//const printTgt=document.getElementById("printTarget");
+
+const dogImgDiv = document.getElementById("dogImgDiv");
+const btnDog = document.getElementById("btnDog");
+
+btnDog.addEventListener("click", getInfo);
+
+function getInfo(){
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response=>response.json())
+        .then(data=>generateImageFromURL(data.message));
+        
+}
+    
+        
+        
+        
+
+
+function generateImageFromURL(dogUrl)
+{
+    const html= `
+        <img style="height:200px;width:200px;" src=${dogUrl} alt="${dogUrl}">
+        <p> Click for another!</p>`;
+    
+    dogImgDiv.innerHTML=html;
+    
+    
+}
+
+function errorReport()
+{
+    alert("Something went wrong!");
+}
+
